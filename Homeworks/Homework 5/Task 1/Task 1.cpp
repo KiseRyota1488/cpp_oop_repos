ef20ab6f70d4c95da134a9217ce93f753347e6f4
KiseRyota1488 <shavaryn_ak19@nuwm.edu.ua>
2020-12-25 20:13:59 +0200

    1.02

--- Homeworks/Homework 5/Task 1/Task 1.cpp
+++ Homeworks/Homework 5/Task 1/Task 1.cpp
@@ -4,29 +4,76 @@
 
 using namespace std;
 
-template <typename T/*, typename T2, typename T3*/>
+template <typename T1, typename T2, typename T3>
 class CShop
 {
 private:
-	vector <T> arr1;
-	vector <T> arr2;
-	vector <T> arr3;
+	vector <T1> arr1;
+	vector <T2> arr2;
+	vector <T3> arr3;
 public:
-	CShop(vector <T>& arr1/*, T* arr2, T* arr3*/)
+	CShop(vector <T1>& arr1, vector <T2> arr2, vector <T3> arr3)
 	{
 		this->arr1 = arr1;
+		this->arr2 = arr2;
+		this->arr2 = arr2;
 	}
 	CShop() {}
 
-	void CheckForNew(vector <T>& arr)
+	void CheckForNew(vector <T1>& arr1, vector <T2>& arr2, vector <T3>& arr3)
 	{
-		for (int i = 0; i < arr.size(); i++)
+		for (int i = 0; i < arr1.size(); i++)
 		{
-			if (i < arr.size() - 2)
-				arr[i].SetIsNew(0);
+			if (i < arr1.size() - 1)
+				arr1[i].SetIsNew(0);
 			else
-				arr[i].SetIsNew(1);
+				arr1[i].SetIsNew(1);
 		}
+		for (int i = 0; i < arr2.size(); i++)
+		{
+			if (i < arr2.size() - 1)
+				arr2[i].SetIsNew(0);
+			else
+				arr2[i].SetIsNew(1);
+		}
+		for (int i = 0; i < arr3.size(); i++)
+		{
+			if (i < arr3.size() - 1)
+				arr3[i].SetIsNew(0);
+			else
+				arr3[i].SetIsNew(1);
+		}
+
+	}
+
+	void SortByPriceExp(vector<T1>& arr1, vector<T2>& arr2, vector<T3>& arr3)
+	{
+		int id = 0, max = -99999;
+
+		for (int i = 0; i < arr1.size(); i++)
+			if (arr1[i].GetPrice() > max)
+			{
+				id = i;
+				max = arr1[i].GetPrice();
+			}
+		max = -99999;
+		arr1[id].Print();
+		for (int i = 0; i < arr2.size(); i++)
+			if (arr2[i].GetPrice() > max)
+			{
+				id = i;
+				max = arr2[i].GetPrice();
+			}
+		max = -99999;
+		arr2[id].Print();
+		for (int i = 0; i < arr3.size(); i++)
+			if (arr3[i].GetPrice() > max)
+			{
+				id = i;
+				max = arr3[i].GetPrice();
+			}
+		arr3[id].Print();
+
 
 	}
 
@@ -39,16 +86,104 @@ public:
 				this->arr1[i].Print();
 		}
 		else if (key == '2')
+		{
+			cout << "Disk name | Author | Disk amount |Price | New\n";
 			for (int i = 0; i < arr2.size(); i++)
 				this->arr2[i].Print();
+		}
 		else if (key == '3')
+		{
+			cout << "Magazine | Price | New\n";
 			for (int i = 0; i < arr3.size(); i++)
 				this->arr3[i].Print();
+		}
+		else if ( key == '4')
+		{
+			cout << "Book name | Author | Price | New\n";
+			for (int i = 0; i < arr1.size(); i++)
+				this->arr1[i].Print();
+			cout << "Disk name | Author | Disk amount |Price | New\n";
+			for (int i = 0; i < arr2.size(); i++)
+				this->arr2[i].Print();
+			cout << "Magazine | Price | New\n";
+			for (int i = 0; i < arr3.size(); i++)
+				this->arr3[i].Print();
+		}
 		else
 			cout << "Wrong command!\n";
 	}
 };
 
+class Audio
+{
+private:
+	string name;
+	string author;
+	short diskAmount;
+	int price;
+	bool isNew;
+public:
+	Audio(string name, string author, short diskAmount, int price)
+	{
+		this->name = name;
+		this->author = author;
+		this->diskAmount = diskAmount;
+		this->price = price;
+	}
+	Audio() {}
+
+	void SetIsNew(bool isNew)
+	{
+		this->isNew = isNew;
+	}
+	bool GetIsNew()
+	{
+		return isNew;
+	}
+	int GetPrice()
+	{
+		return price;
+	}
+
+	
+
+	void Print()
+	{
+		cout << left << setw(10) << name << " " << setw(8) << author << " " << setw(7) 
+			<< diskAmount << " " << setw(7) << price << " " << isNew << endl;
+	}
+};
+class Magazine
+{
+private:
+	string name;
+	int price;
+	bool isNew;
+public:
+	Magazine(string name, int price)
+	{
+		this->name = name;
+		this->price = price;
+	}
+	Magazine() {}
+
+	void SetIsNew(bool isNew)
+	{
+		this->isNew = isNew;
+	}
+	bool GetIsNew()
+	{
+		return isNew;
+	}
+	int GetPrice()
+	{
+		return price;
+	}
+	void Print()
+	{
+		cout << left << setw(10) << name << " "  << setw(8) << price << " " << isNew << endl;
+	}
+};
 class Book
 {
 private:
@@ -73,7 +208,10 @@ public:
 	{
 		return isNew;
 	}
-
+	int GetPrice()
+	{
+		return price;
+	}
 	void Print()
 	{
 		cout << left << setw(10) << name << " " << setw(8) << author << " " << setw(7) << price << " " << isNew << endl;
@@ -83,32 +221,58 @@ public:
 int main()
 {
 	vector <Book> books;
-	/*char key = '0';
+	vector <Audio> disks;
+	vector <Magazine> magazines;
+
+	CShop<Book, Audio, Magazine>shop(books, disks, magazines);
+
+	Book b("row", "gar", 500);
+	books.push_back(b);
+
+	Audio a("cd", "ac", 15, 100);
+	Audio a2("cd", "ac", 25, 200);
+	disks.push_back(a);
+
+	Magazine m("cosmo", 50);
+	magazines.push_back(m);
+
+	char key = '0';
 	do{
-		cout << "CShop\n 1 Show all products\n 2 Show the most expensive product from each category\n"
-			<< "3 Show the cheapest product from each category\n 4 Show avarage price from each category\n"
-			<< "5 Show the HOT new one\n 6 Exit\n->_";
+
+		cout << "CShop\n 1 Show products\n 2 Show the most expensive product from each category\n"
+			<< " 3 Show the cheapest product from each category\n 4 Show avarage price from each category\n"
+			<< " 5 Show the new one\n 6 Sort with price all categories\n 7 Exit\n->_";
 		cin >> key;
 
+		switch (key)
+		{
+		case '1':
+		{
+			system("cls");
+
+			char choice;
+
+			cout << "What category? (1 - Books, 2 - Disks, 3 - Magazines, 4 - All): ";
+			cin >> choice;
+
+			shop.Show(choice);
+
+			break;
+		}
+		case '2':
+		{
+			shop.SortByPriceExp(books, disks, magazines);
 
 
-	} while (key != '6');*/
+			break;
+		}
+		default:
+			break;
+		}
 
-	Book b1("gary", "joanne", 500);
-	Book b2("potter", "rowling", 400);
-	Book b3("prikvel", "idk", 1000);
-
-	books.push_back(b1);
-	books.push_back(b2);
-	books.push_back(b3);
-
-	CShop<Book>shop(books);
-
-	shop.CheckForNew(books);
-	shop.Show('1');
-	cout << books[2].GetIsNew();
-
-	
+		system("PAUSE");
+		system("cls");
+	} while (key != '7');
 
 	return 0;
 }
