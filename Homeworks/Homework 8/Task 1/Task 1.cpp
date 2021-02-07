128f429547ccb986c7e64a022d4c3eedf2fe858e
KiseRyota1488 <shavaryn_ak19@nuwm.edu.ua>
2021-02-07 13:15:13 +0200

    lesson7

---
+++ Homeworks/Homework 8/Task 1/Task 1.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	//1
+	vector <int> v1;
+	v1.resize(5);
+	for (int i = 0; i < v1.size(); i++)
+	{
+		cout << "Enter " << i + 1 << " el: ";
+		cin >> v1[i];
+	}
+
+	vector <int> v2(10, 0);
+
+	vector <int> v3;
+	cout << "Enter v3 size: ";
+	int size;
+	cin >> size;
+	for (int i = 0; i < size; i++)
+		v3.push_back(rand() % 10 + 1);
+
+	vector <int> v4(v1.begin(), v1.end());
+
+	vector <int> v5(v3.size() - 2);
+
+	for (int i = 0; i < v5.size(); i++)
+	{
+		v5[i] = v3[i + 1];
+	}
+
+
+
+	//2
+	for (int i = 0; i < v3.size(); i++)
+	{
+		cout << v3[i] << " ";
+	}
+	cout << endl;
+
+	ofstream write("numbers.txt");
+
+	for (int i = 0; i < v1.size(); i++)
+	{
+		write << v3[i] << " ";
+	}
+
+	write.close();
+
+
+	//3
+
+	for (int i = 0; i < v2.size(); i++)
+	{
+		v2[i] = i + 1;
+	}
+
+	for (int i = 0; i < v2.size(); i++)
+	{
+		swap(v2[i], v2[rand() % v2.size()]);
+	}
+
+	v2.erase(v2.begin() + 1);
+	v2.pop_back();
+
+
+	for (int i = 0; i < v2.size(); i++)
+	{
+		cout << v2[i] << " ";
+	}
+
+	//4
+
+	for (int i = 0; i < v4.size(); i+=2)
+	{
+		v4.push_back(v4[i]);
+		for (int j = v4.size()-1; j > i + 1; j--)
+		{
+			swap(v4[j], v4[j - 1]);
+		}
+	}
+
+	cout << endl;
+	for (int i = 0; i < v4.size(); i++)
+	{
+		cout << v4[i] << " ";
+	}
+
+	system("PAUSE");
+
+}
---
+++ Lessons/Lesson 7/Task 1/Task 1.cpp
@@ -0,0 +1,183 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Library
+{
+private:
+	string name;
+	string author;
+	bool issued;
+	int year;
+
+public:
+	Library(string name, string author, int year)
+	{
+		this->name = name;
+		this->author = author;
+		this->year = year;
+		this->issued = 0;
+	}
+
+	string GetName()
+	{
+		return name;
+	}
+
+	string GetAuthor()
+	{
+		return author;
+	}
+
+	bool GetIssued()
+	{
+		return issued;
+	}
+
+	int GetYear()
+	{
+		return year;
+	}
+
+	void PrintBook()
+	{
+		cout << name << " | Author: " << author << " / " << year << " y. | Issued ";
+		if (issued)
+			cout << "+";
+		else
+			cout << "-";
+
+		cout << endl;
+	}
+
+	void SetIssue(bool issued)
+	{
+		this->issued = issued;
+	}
+
+};
+
+void SortByAuthor(list <Library>&);
+
+void Issue(Library&);
+
+
+int main()
+{
+	list <Library> books;
+
+	char key;
+
+	do {
+		cout << "Menu\n 1. Add book \n 2. Show all library\n->_";
+		cin >> key;
+
+		switch (key)
+		{
+		case '4':
+		{
+			Library book("ab", "bb", 2000);
+			Library book1("aa", "aa", 2000);
+			Library book2("bb", "aa", 2000);
+
+			books.push_back(book);
+			books.push_back(book1);
+			books.push_back(book2);
+
+			break;
+		}
+		case '1':
+		{
+			string name, author;
+			int year;
+
+			cout << "Enter name: ";
+			cin >> name;
+			cout << "Enter author: ";
+			cin >> author;
+			cout << "Enter year: ";
+			cin >> year;
+
+			Library book(name, author, year);
+			books.push_back(book);
+
+			break;
+		}
+		case '2':
+		{
+			if (!books.empty())
+			{
+				for (auto i = books.begin(); i != books.end(); i++)
+				{
+					i->PrintBook();
+				}
+			}
+
+			break;
+		}
+		case '3':
+		{
+			char choice;
+			cout << "Search by name or author: (1 - name/2 - author): ";
+			cin >> choice;
+
+			if (choice == '1')
+			{
+				string name;
+				cout << "Enter name: ";
+				cin >> name;
+				for (auto i = books.begin(); i != books.end(); i++)
+				{
+					if (i->GetName() == name)
+					{
+						i->PrintBook();
+						if (!i->GetIssued())
+							Issue(*i);
+					}
+				}
+			}
+			else if (choice == '2')
+			{
+				string author;
+				cout << "Enter author: ";
+				cin >> author;
+				for (auto i = books.begin(); i != books.end(); i++)
+				{
+					if (i->GetAuthor() == author)
+					{
+						i->PrintBook();
+						if (!i->GetIssued())
+							Issue(*i);
+					}
+				}
+			}
+
+			break;
+		}
+		default:
+			break;
+		}
+		system("PAUSE");
+		system("cls");
+
+	} while (key != '5');
+
+}
+
+void SortByAuthor(list <Library>& books)
+{
+	for (auto i = books.begin(); i != books.end(); i++)
+	{
+
+	}
+}
+
+void Issue(Library& book)
+{
+	bool choice;
+	cout << "Want to take book? 1 yes/0 no: ";
+	cin >> choice;
+	book.SetIssue(choice);
+}
+
